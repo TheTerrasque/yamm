0c8429fe2a9fc7b85a29e87f7d33d67fa2ca0a15
Terrasque <terrasque@thelazy.net>
2015-05-09 02:46:17 +0200

    Mod Organizer plugin starting to work

--- lib/yamm_mo_shim.py
+++ lib/yamm_mo_shim.py
@@ -1,34 +1,82 @@
 import json
 import mmap
 import time
+import tempfile
+import os.path
 
-filename = r"C:\\Users\\Mikael\\yamm_mo_rpc"
+filename = "yamm_mo_rpc"
+filename = os.path.join(tempfile.gettempdir(), filename)
 size=16384
+
+class TimeoutException(Exception):
+    pass
 
 class RpcCaller(object):
     def __init__(self):
         fn = open(filename, "r+")
         self.data = mmap.mmap(fn.fileno(), size)
     
-    def _send_request(self, function, args=[], kwargs={}):
+    def __getattr__(self, value):
+        def caller(*args, **kwargs):
+            return self._send_request(value, args, kwargs)
+        return caller
+        
+    
+    def _send_request(self, function, args=[], kwargs={}, timeout = 0):
         d = {
             "function": function,
             "args": args,
             "kwargs": kwargs
         }
-        jdata = json.dumps(d)
-        self.data.seek(0)
-        tval = jdata + (" " * size)
-        tval = tval[:size]
-        self.data.write(tval)
-        while not "result" in d:
-            time.sleep(0.1)
+        WAIT = 0.01
+        
+        def write(data):
+            self.data.seek(0)
+            tval  = data + (" " * size)
+            tval = tval[:size]
+            self.data.write(tval)
+        
+        def read():
             self.data.seek(0)
             dd = self.data.read(size).strip()
             d = json.loads(dd)
+            return d
+        
+        
+        jdata = json.dumps(d)
+        
+        write(jdata)
+        C = 0
+        while not "result" in d:
+            time.sleep(WAIT)
+            C += WAIT
+            if timeout and C > timeout:
+                raise TimeoutException("Timed out waiting for answer")
+            d = read()
+            
+        if "error" in d:
+            print " ", d["error"]
+            print " ", d["details"]
+            return None
         return d["result"]
         
+        
+rpc = RpcCaller()
+
+def ping():
+    try:
+        return rpc._send_request("version", timeout=0.2)
+    except TimeoutException:
+        return False
+        
+
 if __name__ == "__main__":
-    rpc = RpcCaller()
-    print rpc._send_request("echo", [], {})
-    print rpc._send_request("get_gamename")
+    if ping():
+        print rpc.version()
+        print rpc.get_debug()
+        print rpc.get_gamename()
+        print rpc.get_active_profile()
+        print rpc.install_mod(r"D:\\JContainers-49743-3-2-3.zip")
+        print rpc.install_mod2("JCTEST", r"D:\\JContainers-49743-3-2-3.zip")
+    else:
+        print "Timeout, ModOrganizer is not running?"
--- utils/plugin_MO.py
+++ utils/plugin_MO.py
@@ -12,7 +12,7 @@
 
 # qt5
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import Qt, pyqtWrapperType, pyqtSlot, pyqtSignal
+from PyQt5.QtCore import Qt, pyqtWrapperType, pyqtSlot, pyqtSignal, QTimer
 from PyQt5.QtGui import QColor, QPalette
 from PyQt5.QtWidgets import QDialog, QHeaderView, QMessageBox, QColorDialog, QTreeWidgetItem,\
     QComboBox, QPushButton, QDoubleSpinBox, QHBoxLayout, QWidget, QSlider, QSpinBox, QLineEdit
@@ -20,84 +20,110 @@ from PyQt5.QtWidgets import QDialog, QHeaderView, QMessageBox, QColorDialog, QTr
 if "mobase" not in sys.modules:
     import mock_mobase as mobase
     
-import threading
 import logging
+import os
 
 import json
-import time
 import mmap
+import tempfile
 
 L = logging.getLogger("YAMM.YammMOshim")
 
-def start_rpc(organizer):
-    filename = r"C:\\Users\\Mikael\\yamm_mo_rpc"
-    size=16384
-    
-    class RpcFunctions:
-        def echo(self):
-            return "Hello"
-        
-        def get_gamename(self):
-            return str(organizer.gameInfo().type())
+MMSIZE=16384
+VERSION = 1
 
-    funcs = RpcFunctions()
+class RpcFunctionMMAP(object):
+    _OLD = ""
     
-    def handle_data(data):
+    def __init__(self, organizer, filename="yamm_mo_rpc"):
+        self._organizer = organizer
+        self._fn = filename
+        self._setup_rpc()
+        
+    def _setup_rpc(self):
+        self._filename = os.path.join(tempfile.gettempdir(), self._fn)
+
+        if not os.path.exists(self._filename):
+            with open(self._filename, "wb") as f:
+                f.write("Hello Python!\n")
+
+        fn = open(self._filename, "r+")
+        self._mmap = mmap.mmap(fn.fileno(), MMSIZE)
+    
+    def _poll(self):
+        data = self._read()
+        if data != self._OLD:
+            result = self._handle_call(data)
+            self._write(result)
+        
+    def _write(self, data):
+        self._mmap.seek(0)
+        tval = data + (" " * MMSIZE)
+        tval = tval[:MMSIZE]
+        self._mmap.write(tval)
+        self._OLD = tval
+    
+    def _handle_call(self, data):
         try:
             jdata = json.loads(data)
             funcname = jdata["function"]
             kwargs = jdata.get("kwargs", {})
             args = jdata.get("args", [])
-            result = getattr(funcs, funcname)(*args, **kwargs)
+            result = getattr(self, funcname)(*args, **kwargs)
             jr = json.dumps({"result": result})
-        except:
+        except Exception as e:
             jr = json.dumps({
                 "result": "ERROR",
-                "error": "An error happened"
+                "error": "An error happened",
+                "details": unicode(e)
             })
         return jr
-            
-    
-    with open(filename, "wb") as f:
-        f.write("Hello Python!\n")
-    
-    fn = open(filename, "r+")
-    data = mmap.mmap(fn.fileno(), size)
-    oldval = ""
-    
-    while True:
-        data.seek(0)
-        val = data.read(size)
         
-        if val != oldval:
-            val = handle_data(val)
-            data.seek(0)
-            tval = val + (" " * size)
-            tval = tval[:size]
-            data.write(tval)
-            oldval = tval
-        time.sleep(0.1)
+    def _read(self):
+        self._mmap.seek(0)
+        val = self._mmap.read(MMSIZE).strip()
+        return val
 
-DEBUG = False
+    def __del__(self):
+        self._mmap.close()
+        os.unlink(self._filename)
 
-def start_rpc_thread(organizer):
-    t = threading.Thread(target=start_rpc, args=[organizer])
-    t.daemon = True
-    t.start()
-    return t
+class RpcFunction(RpcFunctionMMAP):
+    
+    def echo(self):
+        return "Hello"
+    
+    def version(self):
+        return VERSION
+    
+    def get_gamename(self):
+        return str(self._organizer.gameInfo().type())
+    
+    def get_debug(self):
+        # organizer / mobase : http://sourceforge.net/p/modorganizer/code/ci/default/tree/source/uibase/imoinfo.h
+        return "Mobase: %s \n\nOrganizer: %s" % (str(dir(mobase)), str(dir(self._organizer)))
+    
+    def get_active_profile(self):
+        return self._organizer.profileName()
+    
+    def install_mod(self, path, name=None, version=None):
+        # Modinstance : http://sourceforge.net/p/modorganizer/code/ci/default/tree/source/uibase/imodinterface.h
+        modinstance = self._organizer.installMod(str(path))
+        if modinstance:
+            return unicode(modinstance.name())
 
-if DEBUG:
-    start_rpc(None)
+    def install_mod2(self, name, path, version=None):
+        r = mobase.IInstallationManager().installArchive(mobase.GuessedString(name), str(path))
+        #mod.setInstallationFile(str(path))
 
 class RpcWindow(QDialog):
     saveSettings = pyqtSignal(dict)
 
-    def __init__(self,  settings,  parent=None):
+    def __init__(self,  organizer,  parent=None):
         super(RpcWindow,  self).__init__(parent)
         
-        self.__organizer = settings
-        start_rpc_thread(settings)
-        
+        self.__organizer = organizer
+
         from pyCfgDialog import Ui_PyCfgDialog
 
         self.__ui = Ui_PyCfgDialog()
@@ -105,6 +131,8 @@ class RpcWindow(QDialog):
         self.setWindowFlags(self.windowFlags() & ~Qt.WindowContextHelpButtonHint)
         self.__lastSelectedCategory = ""
 
+RPC=None
+        
 class IniEdit(mobase.IPluginTool):
 
     def __init__(self):
@@ -118,11 +146,19 @@ class IniEdit(mobase.IPluginTool):
         import pyCfgResource_rc  # required to make icons available
         self.__organizer = organizer
         self.__window = None
-        start_rpc_thread(organizer)
+        #start_rpc_thread(organizer)
+        
+        global RPC
+        if not RPC:
+            RPC = RpcFunction(organizer)
+            self.rpc_poll_timer = QTimer()
+            self.rpc_poll_timer.timeout.connect(RPC._poll)
+            self.rpc_poll_timer.start(20)
+        
         return True
 
     def name(self):
-        return "YAMMClient"
+        return "YAMM Client"
 
     def author(self):
         return "Terrasque"
@@ -131,7 +167,7 @@ class IniEdit(mobase.IPluginTool):
         return "Installs mods and doesn't afraid of anything"
 
     def version(self):
-        return mobase.VersionInfo(1, 0, 0, mobase.ReleaseType.final)
+        return mobase.VersionInfo(1, 0, VERSION, mobase.ReleaseType.final)
 
     def isActive(self):
         return True
@@ -152,8 +188,9 @@ class IniEdit(mobase.IPluginTool):
         self.__parentWidget = widget
 
     def display(self):
-        self.__window = RpcWindow(self.__organizer)
-        self.__window.exec_()
+        QMessageBox.information(self.__parentWidget, "Plugin running", "YAMM plugin is running")
+        #self.__window = RpcWindow(self.__organizer)
+        #self.__window.exec_()
         
 def createPlugin():
     return IniEdit()
