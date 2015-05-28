021bf5c795220af696b485b2392eaf3a6edb307c
Terrasque <terrasque@thelazy.net>
2015-05-28 17:26:05 +0200

    MO plugin install, installer can now start with admin privs

--- setup yamm.py
+++ setup yamm.py
@@ -1,13 +1,15 @@
 import os
 import _winreg as winreg
 from lib.utils import get_base_path
+import shutil
 
 def setup_registry():
+    print "Installing URL handler.."
     python = r"C:\Python27\pythonw.exe"
     this = os.path.join(get_base_path(), "yammy ui.pyw")
     
     if not os.path.exists(python):
-        L.info("Could not find python at %s", python)
+        print("Could not find python at %s" % python)
         return
     
     yamm = winreg.CreateKey(winreg.HKEY_CLASSES_ROOT, r"yamm")
@@ -23,5 +25,31 @@ def setup_registry():
     
     print "URL Handler installed"
 
+def setup_modorganizer():
+    paths = [
+        r"C:\Program Files (x86)\Mod Organizer",
+        r"C:\Program Files\Mod Organizer",
+    ]
+    exe = "ModOrganizer.exe"
+    
+    script = "plugin_MO.py"
+    
+    src = os.path.join(get_base_path(), "utils", script)
+    
+    for path in paths:
+        full = os.path.join(path, exe)
+        if os.path.exists(full):
+            print "MO Found at", path
+            target = os.path.join(path, "plugins", script)
+            shutil.copyfile(src, target)
+            print "MO plugin installed"
+            
 if __name__ == "__main__":
-    setup_registry()
+    print "Starting setup of YAMM\n\n"
+    try:
+        setup_registry()
+        print ""
+        setup_modorganizer()
+    except Exception as e:
+        print e
+    raw_input("\nInstall finished")
---
+++ setup_helper.vbs
@@ -0,0 +1,10 @@
+Set oShell = CreateObject("Shell.Application")
+Set objFSO = CreateObject("Scripting.FileSystemObject")
+
+strPath = Wscript.ScriptFullName
+Set objFile = objFSO.GetFile(strPath)
+strFolder = objFSO.GetParentFolderName(objFile)
+
+MsgBox "This will install YAMM url handler and the MO plugin." & vbcrlf & vbcrlf & "Administrator access is required for this, so next screen will ask for that",64,"YAMM install"
+
+oShell.ShellExecute "c:\python27\python.exe", """" & strFolder & "\setup yamm.py""", "", "runas", 1
